55a233139d42594d93874f228063a10b240a3173
aho4ahoaho <aho4ahoaho@users.noreply.github.com>
2024-03-03 15:33:34 +0900

    preloadからはlogを吐かないように変更

--- main/preload.ts
+++ main/preload.ts
@@ -1,16 +1,12 @@
 import { contextBridge, ipcRenderer, IpcRendererEvent } from "electron";
-import log from "electron-log/main";
-log.initialize();
 
 const handler = {
     send<T>(channel: string, value: T) {
-        log.debug(`Sending ${channel}`);
         ipcRenderer.send(channel, value);
     },
     on<T>(channel: string, callback: (...args: T[]) => void) {
         const subscription = (_event: IpcRendererEvent, ...args: T[]) => callback(...args);
         ipcRenderer.on(channel, subscription);
-        log.debug(`Subscribed to ${channel}`);
 
         return () => {
             ipcRenderer.removeListener(channel, subscription);
